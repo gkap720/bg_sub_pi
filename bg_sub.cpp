f900566c23a48877e748ab8d2f0621c3e3c30648
gkap720 <greg.m.kappes@gmail.com>
2018-10-14 17:14:10 +0200

    white balance, exposure

--- bg_sub.cpp
+++ bg_sub.cpp
@@ -2,21 +2,8 @@
 #include <iostream>
 #include <raspicam/raspicam_cv.h>
 #ifdef HAVE_OPENCV_CONTRIB
-#include <opencv2/bgsegm.hpp>
-using namespace cv::bgsegm;
-#endif
-#include "bgsubcnt.h"
 using namespace cv;
 using namespace std;
-const string keys =
-        "{help h usage ? || print this message}"
-        "{type           |CNT| bg subtraction type from - CNT/MOG2/KNN"
-#ifdef HAVE_OPENCV_CONTRIB
-        "/GMG/MOG"
-#endif
-        "}"
-        "{bg             || calculate also the background}"
-        "{nogui          || run without GUI to measure times}";
 
 bool compareContourAreas ( std::vector<cv::Point> contour1, std::vector<cv::Point> contour2 ) {
     double i = fabs( contourArea(cv::Mat(contour1)) );
@@ -29,74 +16,16 @@ int main( int argc, char** argv )
     cap.set( CV_CAP_PROP_FORMAT, CV_8UC1 );
     cap.set(CAP_PROP_FRAME_WIDTH, 640);
     cap.set(CAP_PROP_FRAME_HEIGHT, 480);
-    CommandLineParser parser(argc, argv, keys);
-    parser.about("cv::bgsubcnt::BackgroundSubtractorCNT demo/benchmark/comparison");
-    if (parser.has("help"))
-    {
-        parser.printMessage();
-        return 0;
-    }
-    bool hasGui = ! parser.has("nogui");
-    bool bgImage = parser.has("bg");
-    string type = parser.get<string>("type");
-    string filePath;
-    if (! parser.check())
-    {
-        parser.printErrors();
-        return 1;
-    }
+    cap.set(RASPICAM_AWB, RASPICAM_AWB_OFF);
+    cap.set(RASPICAM_EXPOSURE, RASPICAM_EXPOSURE_OFF);
     if( !cap.open() )
     {
         cout << "Could not initialize capturing...\n";
         return 0;
     }
-    Ptr<BackgroundSubtractor> pBgSub;
-    if (type == "CNT")
-    {
-        int fps = 15;
-        pBgSub = cv::bgsubcnt::createBackgroundSubtractorCNT(fps, true, fps*60);
-    }
-    else if (type == "MOG2")
-    {
-        Ptr<BackgroundSubtractorMOG2> pBgSubMOG2 = createBackgroundSubtractorMOG2();
-        pBgSubMOG2->setDetectShadows(false);
-        pBgSub = pBgSubMOG2;
-    }
-    else if (type == "KNN")
-    {
-        pBgSub = createBackgroundSubtractorKNN();
-    }
-#ifdef HAVE_OPENCV_CONTRIB
-    else if (type == "GMG")
-    {
-        pBgSub = createBackgroundSubtractorGMG();
-    }
-    else if (type == "MOG")
-    {
-        pBgSub = createBackgroundSubtractorMOG();
-    }
-#endif
-    else
-    {
-        parser.printMessage();
-        cout << "\nWrong type - please see above\n";
-        return 1;
-    }
-    bool showFG=true;
-    if (hasGui)
-    {
-        namedWindow("FG", 1);
-        if (bgImage)
-        {
-            namedWindow("BG", 1);
-        }
-        cout << "Press 's' to save a frame to the current directory.\n"
-                "Use ESC to quit.\n" << endl;
-    }
     int64 startTime = getTickCount();
-    Mat kernel, avg, frameDelta;
+    Mat avg, frameDelta;
     bool init = false;
-    kernel = cv::getStructuringElement(MORPH_ELLIPSE, Size(5,5));
     for(;;)
     {
         Mat frame, fgMask, fg;
@@ -119,11 +48,11 @@ int main( int argc, char** argv )
         accumulateWeighted(frame, avg, 0.5);
         convertScaleAbs(avg, avg);
         absdiff(frame, avg, frameDelta);
+        threshold( frameDelta, frameDelta, 0, 255, 0 );
         vector<vector<Point> > contours0;
         findContours( frameDelta, contours0, RETR_TREE, CHAIN_APPROX_SIMPLE);
         std::sort(contours0.begin(), contours0.end(), compareContourAreas);
         Scalar color( 255,0,0);
-        threshold( frameDelta, frameDelta, 0, 255, 0 );
         cvtColor(frameDelta, frameDelta, cv::COLOR_GRAY2BGR);
         if(contours0.size()>1) {
             int cX, cY;
