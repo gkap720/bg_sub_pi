60da830fbcce7ecece35e42b163128496651fc46
gkap720 <greg.m.kappes@gmail.com>
2018-10-14 17:02:21 +0200

    threshold

--- bg_sub.cpp
+++ bg_sub.cpp
@@ -123,17 +123,18 @@ int main( int argc, char** argv )
         findContours( frameDelta, contours0, RETR_TREE, CHAIN_APPROX_SIMPLE);
         std::sort(contours0.begin(), contours0.end(), compareContourAreas);
         Scalar color( 255,0,0);
-        cvtColor(frame, frame, cv::COLOR_GRAY2BGR);
+        threshold( frameDelta, frameDelta, 0, 255, 0 );
+        cvtColor(frameDelta, frameDelta, cv::COLOR_GRAY2BGR);
         if(contours0.size()>1) {
             int cX, cY;
             cv::Moments M;
             M = moments(contours0[contours0.size()-1]);
             cX = int(M.m10 / M.m00);
             cY = int(M.m01 / M.m00);
-            circle(frame, Point(cX, cY), 7, color, -1);
+            circle(frameDelta, Point(cX, cY), 7, color, -1);
             //drawContours( frame, contours0, contours0.size()-1, color, CV_FILLED, 8 );
         }
-        imshow("FG", frame);
+        imshow("FG", frameDelta);
         if (hasGui)
         {
             char c = (char)waitKey(1);
