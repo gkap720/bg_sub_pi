7db797cb8e04d662c9e915962139604f5f07c0d0
gkap720 <greg.m.kappes@gmail.com>
2018-10-14 19:12:09 +0200

    serial

--- bg_sub.cpp
+++ bg_sub.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <wiringSerial.h>
 #include <raspicam/raspicam_cv.h>
 using namespace cv;
 using namespace std;
@@ -9,6 +10,7 @@ bool compareContourAreas ( std::vector<cv::Point> contour1, std::vector<cv::Poin
     double j = fabs( contourArea(cv::Mat(contour2)) );
     return ( i < j );
 }
+
 int main( int argc, char** argv )
 {
     raspicam::RaspiCam_Cv cap;
@@ -17,6 +19,10 @@ int main( int argc, char** argv )
     cap.set(CAP_PROP_FRAME_HEIGHT, 480);
     cap.setAWB(0);
     cap.set(CAP_PROP_EXPOSURE, 0);
+    int fd = serialOpen ("/dev/ttyUSB0", 115200) ;
+    if (fd < 0)
+        cout << "Error opening serial\n";
+    
     int thresh = 50;
     float speed = 0.5;
     if( !cap.open() )
@@ -61,13 +67,14 @@ int main( int argc, char** argv )
         cvtColor(frameDelta, frameDelta, cv::COLOR_GRAY2BGR);
         drawContours( frameDelta, contours0, 0, color, CV_FILLED, 8);
         if(contours0.size() > 0) {
-            int cX, cY;
+            int cX, cY, n;
             cv::Moments M;
             M = moments(contours0[0]);
             cX = int(M.m10 / M.m00);
             cY = int(M.m01 / M.m00);
             circle(frameDelta, Point(cX, cY), 7, color, -1);
             cout << cX << endl;
+            serialPutchar(fd, cX);
         }
         imshow("FG", frameDelta);
         waitKey(30);
